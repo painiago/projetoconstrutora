de0c089819bc23fedbd350d0533089c83e843725
painiago <95057970+painiago@users.noreply.github.com>
2023-10-23 15:38:02 -0300

    att caminho folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,9 @@ import { ToastContainer} from 'react-toastify';
 import dynamic from 'next/dynamic';
 import Alert from '@/components/Alert/alert';
 import ScrollTop from '@/components/ScrollBtn/ScrollToTopButton';
+import ContatoHome from '@/components/contatohome/contatoHome';
 
-const ContatoHomeComponent = dynamic (() => import('@/components/contatohome/contatoHome'));
+// const ContatoHomeComponent = dynamic (() => import('@/components/contatohome/contatoHome'));
 // const ScrollTop = dynamic(() => import('@/components/ScrollBtn/ScrollToTopButton'));
 // const AlertComp = dynamic (() => import ('@/components/Alert/alert'))
 
@@ -159,7 +160,7 @@ export default function Home() {
           </span>
       </div>
     </section>
-    <ContatoHomeComponent/>
+    <ContatoHome/>
     </main>
   )
 }
